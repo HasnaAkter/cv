d8c6d633edc180a00d026b6932cec6f83628ca07
HasnaAkter <hasnaakter.cse@gmail.com>
2023-04-26 20:21:06 +0600

    CountingNumberreact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Counting></Counting>
+     
     </div>
   );
 }
+
+
+function Counting(){
+
+  const [count, setCount] = useState(50);
+  // console.log(abc);
+  // const increaseCount = () =>{
+  //   const newCount = count+1;
+  //   setCount(newCount);
+  // }
+  const increaseCount = () =>setCount(count+1);
+  const decreaseCount = () =>setCount(count-1);
+
+
+
+  return(
+    <div>
+          
+          <h1>Count Number:{count} </h1>
+          <button style={ {background:"red" , padding:"10px" , borderRadius:"10px "} } onClick={increaseCount}>Increase</button>
+          <button style={ {background:"red" , padding:"10px" , borderRadius:"10px ", margin:"20px"} } onClick={decreaseCount}>Decrease</button>
+
+    </div>
+  );
+}
+
+
 
 export default App;
